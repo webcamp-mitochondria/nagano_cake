0c902acb65f90b80b6012e205cb922b0fbe2da18
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-09-18 16:13:36 +0900

    Add View homes top.html.erb

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,14 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items = Item.page(params[:page]).per(8)
+    @all_items = Item.all
+    @genres = Genre.all
   end
 
   def show
+    @item = Item.find(params[:id])
+    @price = @item.price_without_tax
+    @genres = Genre.all
+    @cart_item = CartItem.new
   end
 end
