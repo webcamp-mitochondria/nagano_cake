171f3566bc11fc32d2af58a23d2b5780ab8339d9
TuS2023 <138543864+TuS2023@users.noreply.github.com>
2023-09-19 10:08:36 +0000

    public-item

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,14 +1,20 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.page(params[:page]).per(8)
+    if params[:genre_id]
+      genre = Genre.find(params[:genre_id])
+      @items = genre.items
+    else
+      @items = Item.page(params[:page]).per(8)
+    end
+
     @all_items = Item.all
     @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
-    @price = @item.price_without_tax
+    #@price = @item.price_without_tax
     @genres = Genre.all
-    @cart_item = CartItem.new
+    @cart_item = CartItem
   end
 end
