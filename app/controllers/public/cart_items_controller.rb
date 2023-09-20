93986fd9772bdc7a5ff0aa716579fdf43259c094
nakakoji-hiroto <140766730+nakakoji-hiroto@users.noreply.github.com>
2023-09-20 07:17:19 +0000

    edit_cart

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,6 +5,9 @@ class Public::CartItemsController < ApplicationController
   end
   
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to request.referer
   end
   
   def destroy
