745b3e5664e0eb39db3c100c5b16bdc02fe5267f
TuS2023 <138543864+TuS2023@users.noreply.github.com>
2023-09-23 06:14:49 +0000

    Add public_orders

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,42 +1,66 @@
 class Public::OrdersController < ApplicationController
+
   def new
     @order = Order.new
   end
 
   def index
+    @orders = Order.where(customer_id:[current_customer.id])
   end
 
   def show
+    @order = Order.find(params[:id])
+    if @order.customer_id != current_customer.id
+      redirect_to root_path
+    end
+    @order_details = OrderDetail.where(order_id: @order.id)
   end
 
   def confirm
+    @cart_items = CartItem.where(customer_id: current_customer.id)
+    @delivery_charge = 800
+    @selected_payment_method = params[:order][:payment_method]
+    ary = []
+    @cart_items.each do |cart_item|
+      ary << cart_item.item.with_tax_price*cart_item.amount
+    end
+    @cart_items_price = ary.sum
+
+    @total_price = @delivery_charge + @cart_items_price
     @order = Order.new(order_params)
-    if params[:order][:address_type] == "0"
-      @order.postal_code = current_costomer.postal_code
-      @order.address = current_costomer.address
-      @order.name = current_costomer.first_name + current_costomer.last_name
-    elsif params[:order][:address_type] == "1"
-      @address = Address.find(params[:order][:address_type])
-      @order.postal_code = @address.postal_code
-      @order.address = @address.address
-      @order.name = @address.name
-    elsif params[:order][:address_type] == "2"
-      @order.customer_id = current_customer.id
-    end 
-    @cart_items = current_customer.cart_items
-    @order_new = Order.new
-    render :confirm
-
-  end
-
-  def complete
+    if params[:order][:address_type] == "customer_address"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:address_type] == "registered_address"
+      address = Address.find(params[:order][:address_id])
+      @order.postal_code = address.postal_code
+      @order.address = address.address
+      @order.name = address.name
+    end
   end
 
   def create
+    order = Order.new(order_params)
+    order.save
+    @cart_items = current_customer.cart_items.all
+
+    @cart_items.each do |cart_item|
+      @order_details = OrderDetail.new
+      @order_details.order_id = order.id
+      @order_details.item_id = cart_item.item.id
+      @order_details.purchase_price = cart_item.item.with_tax_price
+      @order_details.amount = cart_item.amount
+      @order_details.production_status = 0
+
+      @order_details.save
+    end
+    @cart_items.destroy_all
+    redirect_to '/orders/complete'
   end
   private
-
   def order_params
-    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :delivery_charge, :amount, :customer_id , :status)
+    params.require(:order).permit(:purchase_price, :postal_code, :address, :name, :paymet_method , :delivery_charge, :amount, :status, :total_price)
   end
 end
+
