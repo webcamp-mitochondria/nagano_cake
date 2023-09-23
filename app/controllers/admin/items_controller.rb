ac74c11fa9eb24abf5d29f760e270b667cc56d6b
maruri6797 <138319041+maruri6797@users.noreply.github.com>
2023-09-23 06:23:36 +0000

    Update

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ItemsController < ApplicationController
   def index
-    @items = Item.page(params[:page]).per(10)
+    @items = Item.all.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def new
