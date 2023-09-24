3e2d09c8762d2057dac082c6f37cfab81aecdeb2
nakakoji-hiroto <140766730+nakakoji-hiroto@users.noreply.github.com>
2023-09-24 07:11:33 +0000

    update_total_layout

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,7 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details
+    @product_total = 0
   end
 
   def update
